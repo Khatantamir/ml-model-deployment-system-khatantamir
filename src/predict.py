10f2021b7fe48726cb312d1576999dc3348c7c14
gurvanbadrakh <hhatnaa690@gmail.com>
2026-03-15 21:00:01 +0800

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,18 @@
+import joblib
+import pandas as pd
 
+
+# Load saved model
+model = joblib.load("models/model.pkl")
+
+
+def predict_survival(data: dict):
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)
+
+    if prediction[0] == 1:
+        return "Survived"
+    else:
+        return "Did not survive"
