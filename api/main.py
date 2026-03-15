fc911ef830c6fa1af708ea22621373d7177e451e
gurvanbadrakh <hhatnaa690@gmail.com>
2026-03-15 21:01:24 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+from src.predict import predict_survival
+
+
+app = FastAPI()
+
+
+class PassengerData(BaseModel):
+    Pclass: int
+    Sex: int
+    Age: float
+    SibSp: int
+    Parch: int
+    Fare: float
+
+
+@app.get("/")
+def home():
+    return {"message": "Titanic ML Model Deployment API is running"}
+
+
+@app.post("/predict")
+def predict(data: PassengerData):
+    result = predict_survival(data.dict())
+    return {"prediction": result}
